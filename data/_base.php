2476d271a523e7c5277929670d80c34566d74586
DrayChou <hnzksq@gmail.com>
2012-10-10 16:42:01 +0800

    first commit

---
+++ data/_base.php
@@ -0,0 +1,30 @@
+﻿<?php
+//用户默认推特设置
+return array(
+    'user_id' => '',
+    'screen_name' => '',
+    'user_info' => array(),
+    'oauths' => array(
+        'used' => 'local',
+        'list' => array(
+            'base' => array(
+                'name' => '大叔家无良的测试机',
+                'description' => '私人用品，严禁冒用，违者必究。',
+                'website' => 'https://dev.twitter.com/apps/new',
+                'key' => 'B2qv3jvJHh9LzbC1fuM7QA',
+                'secret' => 'vEnLo2DMgpkQGvGJMIz6datEoC4G4PqDIWxHAn4x30',
+                'oauth_token' => '',
+                'oauth_token_secret' => '',
+            ),
+            'local' => array(
+                'name' => '大叔家无良的超级测试机本地版',
+                'description' => '个人所有，盗用必究。',
+                'website' => 'https://dev.twitter.com/apps/new',
+                'key' => 'BqZd0OXsXYtYftQZawBxew',
+                'secret' => '1PmRC3UZdo0RguWqdVq2OymRih7uGLaaqfbnKexl3bo',
+                'oauth_token' => '',
+                'oauth_token_secret' => '',
+            ),
+        ),
+    ),
+);
